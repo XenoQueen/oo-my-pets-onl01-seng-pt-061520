3f922b24b01b7f408fb81a3c4c3838e25a675848
Maika Kailani <queenmab051@gmail.com>
2020-07-22 18:06:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
   
+  attr_accessor :owner, :mood 
+  attr_reader :name
+  
+  @@dogs = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = mood
+    owner.dogs << self
+  end
+  
+  def self.all
+    @@dogs
+  end
 end
